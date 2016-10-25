e28ecddfe60fb8b54e38484a34de174bcec14d08
Matthew Robertson <mrmatt1877@gmail.com>
2016-10-25 17:08:07 -0400

    updated article index and article partial styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,13 @@ module ApplicationHelper
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username)
-
+  end
+  def flash_class(level)
+    case level.to_sym
+    when :notice then "light-blue accent-2"
+    when :success then "teal accent-3"
+    when :error then "white-text red darken-3"
+    when :danger then "white-text deep-orange accent-4"
+    end
   end
 end
